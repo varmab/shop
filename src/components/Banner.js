614896fbb9763213d59b7cb7280598466c4b177f
Varma Bhupatiraju <varmab@gmail.com>
2019-05-28 09:29:46 +0530

    Initial version

---
+++ src/components/Banner.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class Banner extends Component{
+    render(){
+        return(
+            <h1>Banner</h1>
+        )
+    }
+}
+
+export default Banner;
---
+++ src/components/Catalog.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class Catalog extends Component{
+    render(){
+        return(
+            <h1>Catalog</h1>
+        )
+    }
+}
+
+export default Catalog;
